3c0adefcfe179a1dc26dea1553bf50a639261b7b
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-05-09 15:30:14 +0000

    [Add]jQuery

--- app2.js
+++ app2.js
@@ -1,29 +1,35 @@
 //ジャンケンの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
-
+//グー、チョキ、パー、キャンセルではないとき（空白時）のアラートの記述
+while((user_hand!="グー")&&(user_hand!="チョキ")&&(user_hand!="パー")&&(user_hand!=null)){
+  alert('グー・チョキ・パーのいずれかを入力してください');
+  user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
+}
 //ジャンケンの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
-
 //ユーザの手とJavaScriptのジャンケンの手を比べる関数を呼び出し、結果をjudgeに入れる
 let judge = winLose(user_hand,js_hand);
-
+//もしジャンケンの手がnull(キャンセル)ではないなら
+if(user_hand!=null){
 //結果を表示する
 alert('あなたの選んだ手は'+ user_hand +'です。\nJavaScriptの選んだ手は'+js_hand+'です。\n結果は'+judge+'です。');
-
+}else{
+  alert("またチャレンジしてね")
+}
 //ランダムでジャンケンの手を作成する関数
 function getJShand(){
   let js_hand_num = Math.floor(Math.random()* 3);
-  let hand_name;
+  let hand;
 
   if(js_hand_num == 0){
-    hand_name = "グー";
+    hand = "グー";
   } else if (js_hand_num == 1){
-    hand_name = "チョキ";
+    hand = "チョキ";
   } else if (js_hand_num == 2){
-    hand_name = "パー";
+    hand = "パー";
   }
 
-  return hand_name;
+  return hand;
   }
 
 //ユーザの手とJavaScriptのジャンケンの手を比べる関数
